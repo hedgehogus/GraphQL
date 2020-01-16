ffef8a27be93a00557051c8bbf03631cb459705c
hedgehogus <research162@gmail.com>
2020-01-16 14:35:01 +0200

    10

--- graphqlProject/backend/server.js
+++ graphqlProject/backend/server.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('hello world'));
+// app.get('/', (req, res) => res.send('hello world'));
+
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
+})); 
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({}));
 
 const port = 4321;
 
-app.listen(port, () => console.log('express rerver running on port 4000'))
+app.listen(port, () => console.log('express server running on port ' + port));
