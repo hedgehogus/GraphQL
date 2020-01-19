60cd56a968bb7c66e92b7ad2070b7da09c14c8d2
LDenysiuk <liudmyla.denysiuk@itrexgroup.com>
2020-01-20 00:09:34 +0200

    32

---
+++ graphqlProject/front/src/app/course.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import {Query, Course} from './types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+
+  constructor() { }
+}
