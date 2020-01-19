8de2395711a15e9701d27d5728329aab3b07bd8b
hedgehogus <research162@gmail.com>
2020-01-20 00:16:21 +0200

    import 33

--- graphqlProject/front/src/app/course.service.ts
+++ graphqlProject/front/src/app/course.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'apollo-angular';
+import { map, filter } from 'rxjs/operators'
 import {Query, Course} from './types';
 
 @Injectable({
